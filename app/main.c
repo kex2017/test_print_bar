bc62bfbd225fac7eba16c04bcf1cf9edb9102269
xuke <xuke@klec.com.cn>
2020-04-28 13:57:17 +0800

    remove unused code

--- app/main.c
+++ app/main.c
@@ -13,19 +13,9 @@ static const shell_command_t shell_commands[] = {
     { NULL, NULL, NULL }
 };
 
-uint8_t id[CPUID_LEN];
-char *test_send = "-----------1";
+char *test_send = "-----------2";
 int main(void)
 {
-    /* read the CPUID */
-    cpuid_get(id);
-     /* print the CPUID */
-    printf("CPUID:");
-    for (unsigned int i = 0; i < CPUID_LEN; i++) {
-        printf(" 0x%02x", id[i]);
-    }
-    printf("\n");
-
     vcc_b_on();
     xtimer_sleep(1);
 
@@ -35,8 +25,6 @@ int main(void)
         xtimer_sleep(3);
         lora_send_message((uint8_t*)test_send, strlen(test_send));
     }
-
-
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
