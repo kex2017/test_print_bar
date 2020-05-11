aa9737885b10b38b333aae22c87ba98fc7cf6ec3
xuke <xuke@klec.com.cn>
2020-05-11 17:03:01 +0800

    ...

--- app/main.c
+++ app/main.c
@@ -27,19 +27,34 @@ int test(void)
     return 0;
 }
 
-
 #include "crypto/aes.h"
 
 static uint8_t TEST_0_KEY[] = {
     0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
-    0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF
-};
+    0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
 
 static uint8_t TEST_0_INP[] = {
-    0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
-    0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+    0x8,
+    0x9,
+    0xA,
+    0xB,
+    0xC,
+    0xD,
+    0xE,
+    0xF,
+    0x0,
+    0x1,
+    0x2,
+    0x3,
+    0x4,
+    0x5,
+    0x6,
+    0x7,
 };
 
+#include "xtimer.h"
+
+#include "progress_bar.h"
 
 int main(void)
 {
@@ -60,15 +75,32 @@ int main(void)
     }
     printf("\r\n");
 
-
     err = aes_decrypt(&ctx, data_encrypted, data_decrypted);
 
-      printf("after decrypt err is %d data is :\r\n", err);
+    printf("after decrypt err is %d data is :\r\n", err);
     for (int i = 0; i < AES_BLOCK_SIZE; i++)
     {
         printf("%02x ", data_decrypted[i]);
     }
     printf("\r\n");
+
+    char *name = "xk";
+    printf("|id\t|name\t|age\t|\r\n");
+    for (int i = 1; i < 100; i += 1)
+    {
+        printf("\r|%d\t|%s\t|%d\t|\n", i,name, 10 + i);
+        if (i % 5 == 0)
+        {
+            printf("\033[5A");
+        }
+
+        xtimer_sleep(1);
+    }
+
+    // char *prefix = "1";
+
+    // char *suffix = "l";
+    // progress_bar_print(prefix, suffix, 3);
 
     return 0;
 }
